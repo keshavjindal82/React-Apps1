3fd233d8b83647871253dacaff4b4a303bab39ae
keshav jindal <4649-2022@WISDOMEDUIN.onmicrosoft.com>
2023-06-04 15:04:09 +0530

    code changes .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Button from './Button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App color-red">
+     Hello world keshav
+     <form>
+      Name: <input type='text' />
+      <Button name="keshav button"></Button>
+     </form>
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+
+function Button(props) {
+  return (
+    <div>
+      <button>{props.name}</button>
+     
+    </div>
+  );
+}
+
+export default Button;
